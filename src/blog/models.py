440c56f609088f50d4dd8715bef6833a41bc6a9d
CodingForEntrepreneurs <hello@teamcfe.com>
2024-07-31 10:13:09 -0600

    9 - Integrate Pgvector & Django

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,9 +1,14 @@
 from django.db import models
-
+from pgvector.django import VectorField
 # Create your models here.
+
+# https://platform.openai.com/docs/guides/embeddings/how-to-get-embeddings
+EMEDDING_MODEL="text-embedding-3-small"
+EMEDDING_LENGTH=1536
+
 class BlogPost(models.Model):
     # id -> models.AutoField()
     title = models.CharField(max_length=200)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    # embedding
+    embedding = VectorField(dimensions=EMEDDING_LENGTH, blank=True, null=True)
